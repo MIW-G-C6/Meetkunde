1e28284c5954291ac11afd85ee7ca7b78c102871
Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
2021-06-15 10:09:34 +0200

    oefening 5.1

--- src/controller/MeetkundeLauncher.java
+++ src/controller/MeetkundeLauncher.java
@@ -13,31 +13,22 @@ import model.Rechthoek;
 public class MeetkundeLauncher {
 
     public static void main(String[] args) {
-        System.out.println(Figuur.geefDefinitie());
-        System.out.println(Cirkel.geefDefinitie());
+        Cirkel mijnCirkel = new Cirkel(3, new Punt(2, 5), "Groen");
+        Rechthoek mijnRechthoek = new Rechthoek(3, 4, new Punt(-2, 6), "Blauw");
 
-        Cirkel[] mijnCirkelArray = new Cirkel[3];
-        mijnCirkelArray[0] = new Cirkel(3, new Punt(1, 4), "groen");
-        mijnCirkelArray[1] = new Cirkel();
-        mijnCirkelArray[2] = new Cirkel(6);
+        Figuur[] figuren = new Figuur[3];
+        figuren[0] = mijnCirkel;
+        figuren[1] = mijnRechthoek;
+        figuren[2] = new Cirkel(10, new Punt(-1, -3), "Karmozijn");
 
-        for (int cirkel = 0; cirkel < mijnCirkelArray.length; cirkel++) {
-            System.out.println(mijnCirkelArray[cirkel]);
-            System.out.println(mijnCirkelArray[cirkel].vertelOverGrootte());;
+        for (Figuur figuur : figuren) {
+            toonInformatie(figuur);
         }
+    }
 
-        System.out.println(Rechthoek.geefDefinitie());
-
-        Rechthoek[] mijnRechthoekArray = new Rechthoek[3];
-
-        mijnRechthoekArray[0] = new Rechthoek(4, 3, new Punt(2, 5), "Blauw");
-        mijnRechthoekArray[1] = new Rechthoek();
-        mijnRechthoekArray[2] = new Rechthoek(25, 10);
-
-        for (Rechthoek rechthoek : mijnRechthoekArray) {
-            System.out.println(rechthoek);
-            System.out.println(rechthoek.vertelOverGrootte());
-        }
+    public static void toonInformatie(Figuur figuur) {
+        System.out.println(figuur);
+        System.out.println();
     }
 
 }
